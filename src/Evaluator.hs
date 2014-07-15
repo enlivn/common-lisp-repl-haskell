7fe1fd5fede5455083b04c545bc1a30ca71d19b6
Ray R <info@enlivn.io>
2014-07-15 18:30:31 -0400

    Adding string comparison functions.

--- src/Evaluator.hs
+++ src/Evaluator.hs
@@ -17,28 +17,39 @@ eval (List (Atom func:rest)) = maybe (throwError (NotAFunction "Unrecognized fun
 primitives :: [(String, ([LispVal] -> ThrowsError LispVal))]
 primitives = [
                 -- numeric operations
-                ("+",         numericOp0OrMoreArgs 0 (+)),  -- zero or more args
-                ("*",         numericOp0OrMoreArgs 1 (*)),  -- zero or more args
-                ("-",         numericOp1OrMoreArgs 0 (-)),  -- one or more args
-                ("/",         numericOp1OrMoreArgs 1 div),  -- one or more args -- TODO: / is NOT in line with CLisp specs,
+                ("+",         numericOp0OrMoreArgs 0 (+)),       -- zero or more args
+                ("*",         numericOp0OrMoreArgs 1 (*)),       -- zero or more args
+                ("-",         numericOp1OrMoreArgs 0 (-)),       -- one or more args
+                ("/",         numericOp1OrMoreArgs 1 div),       -- one or more args -- TODO: / is NOT in line with CLisp specs,
                                                                                 --       which also provide for a Ratio being returned
-                ("/=",        numericOp1OrMoreArgs' (/=)),  -- one or more args
-                ("=",         numericOp1OrMoreArgs' (==)),  -- one or more args
-                ("<",         numericOp1OrMoreArgs' (<)),   -- one or more args
-                (">",         numericOp1OrMoreArgs' (>)),   -- one or more args
-                ("<=",        numericOp1OrMoreArgs' (<=)),  -- one or more args
-                (">=",        numericOp1OrMoreArgs' (>=)),  -- one or more args
-                ("1+",        numericOpNArgs1 (+)),        -- exactly one arg
-                ("1-",        numericOpNArgs1 (-)),        -- exactly one arg
-                ("mod",       numericOpNArgs 2 mod),        -- exactly two args
-                ("rem",       numericOpNArgs 2 rem),        -- exactly two args
+                ("/=",        boolNumericOpOneOrMoreArgs (/=)),  -- one or more args
+                ("=",         boolNumericOpOneOrMoreArgs (==)),  -- one or more args
+                ("<",         boolNumericOpOneOrMoreArgs (<)),   -- one or more args
+                (">",         boolNumericOpOneOrMoreArgs (>)),   -- one or more args
+                ("<=",        boolNumericOpOneOrMoreArgs (<=)),  -- one or more args
+                (">=",        boolNumericOpOneOrMoreArgs (>=)),  -- one or more args
+                ("1+",        numericOnePlusOneMinusOp (+)),     -- exactly one arg
+                ("1-",        numericOnePlusOneMinusOp (-)),     -- exactly one arg
+                ("mod",       numericOpNArgs 2 mod),             -- exactly two args
+                ("rem",       numericOpNArgs 2 rem),             -- exactly two args
 
                 -- boolean operations
-                ("not",       booleanNotOp),                -- exactly one arg
-                ("and",       booleanAndOp),                -- zero or more args
-                ("or",        booleanOrOp)                 -- zero or more args
+                ("not",       booleanNotOp),                     -- exactly one arg
+                ("and",       booleanAndOp),                     -- zero or more args
+                ("or",        booleanOrOp),                      -- zero or more args
+
+                -- string operations
+                ("string=",   boolStringOpTwoArgs (==)),         -- exactly two args
+                ("string/=",  boolStringOpTwoArgs (/=)),         -- exactly two args
+                ("string<",   boolStringOpTwoArgs (<)),          -- exactly two args
+                ("string>",   boolStringOpTwoArgs (>)),          -- exactly two args
+                ("string<=",  boolStringOpTwoArgs (<=)),         -- exactly two args
+                ("string>=",  boolStringOpTwoArgs (>=))          -- exactly two args
              ]
 
+--------------------------------------
+-- ops that return a numeric result.
+--------------------------------------
 numericOp0OrMoreArgs :: Integer -> (Integer -> Integer -> Integer) -> [LispVal] -> ThrowsError LispVal
 numericOp0OrMoreArgs start op l = return . Number =<< (liftM (foldl op start) (mapM extractNumber l))
 
@@ -46,24 +57,37 @@ numericOp1OrMoreArgs :: Integer -> (Integer -> Integer -> Integer) -> [LispVal] 
 numericOp1OrMoreArgs _ _ [] = throwError (NumArgsMismatch ">=1" [])
 numericOp1OrMoreArgs start op l = return . Number =<< (liftM (foldl op start) (mapM extractNumber l))
 
--- no start value provided. the start value is the first element of the [LispVal] sent in (only if it's a number, otherwise
--- extractNumber protects us by throwing a type mismatch error)
-numericOp1OrMoreArgs' :: (Integer -> Integer -> Bool) -> [LispVal] -> ThrowsError LispVal
-numericOp1OrMoreArgs'  _ [] = throwError (NumArgsMismatch ">=1" [])
-numericOp1OrMoreArgs' op l = return . Bool =<< (\x -> return $ foldl (f (head x)) True (tail x)) =<< (mapM extractNumber l)
-    where f :: Integer -> Bool -> Integer -> Bool
-          f _ False _ = False
-          f x _ y | x `op` y = True
-                  | otherwise = False
-
-numericOpNArgs1 :: (Integer -> Integer -> Integer) -> [LispVal] -> ThrowsError LispVal
-numericOpNArgs1 op l | length l /= 1 =  throwError (NumArgsMismatch "= 1" [])
+-- 1+ and 1-
+numericOnePlusOneMinusOp :: (Integer -> Integer -> Integer) -> [LispVal] -> ThrowsError LispVal
+numericOnePlusOneMinusOp op l | length l /= 1 =  throwError (NumArgsMismatch "= 1" [])
                      | otherwise     = return =<< (\x -> return $ Number $ op x 1) =<< extractNumber (head l)
 
+-- mod and rem
 numericOpNArgs :: Int -> (Integer -> Integer -> Integer) -> [LispVal] -> ThrowsError LispVal
 numericOpNArgs n op l | length l /= n = throwError (NumArgsMismatch ("= " ++ show n) l)
                       | otherwise     = return . Number =<< (\x -> return $ foldl op (head x) (tail x)) =<< (mapM extractNumber l)
 
+--------------------------------------
+-- ops that return a boolean result.
+--------------------------------------
+-- generic boolean function that we use to build string or numeric specific function
+genericBoolOpNArgs :: (LispVal -> ThrowsError a) -> Int -> (a -> a -> Bool) -> [LispVal] -> ThrowsError LispVal
+genericBoolOpNArgs _ n _ l | length l /= n = throwError (NumArgsMismatch ("= " ++ show n) l)
+genericBoolOpNArgs extractFunc _ op l = return . Bool =<< (\x -> return $ foldl (f (head x)) True (tail x)) =<< (mapM extractFunc l)
+    where f _ False _ = False
+          f x _ y | x `op` y = True
+                  | otherwise = False
+
+-- >, <, >=, <=, =, /=
+boolNumericOpOneOrMoreArgs :: (Integer -> Integer -> Bool) -> [LispVal] -> ThrowsError LispVal
+boolNumericOpOneOrMoreArgs _ [] = throwError (NumArgsMismatch ">=1" [])
+boolNumericOpOneOrMoreArgs op l  = genericBoolOpNArgs extractNumber (length l) op l -- (length l) effectively bypasses length check in genericBoolOpNArgs
+
+-- string=, string/=, string<, string>, string<=, string>=
+boolStringOpTwoArgs :: (String -> String -> Bool) -> [LispVal] -> ThrowsError LispVal
+boolStringOpTwoArgs = genericBoolOpNArgs extractString 2
+
+-- not
 -- CLisp 'not' works with generalized booleans. Returns true only if passed nil.
 booleanNotOp :: [LispVal] -> ThrowsError LispVal
 booleanNotOp l | length l /= 1 = throwError (NumArgsMismatch "= 1" l)
@@ -71,18 +95,21 @@ booleanNotOp l | length l /= 1 = throwError (NumArgsMismatch "= 1" l)
                 (Bool False) -> return $ Bool True
                 _            -> return $ Bool False
 
+-- or
 booleanOrOp :: [LispVal] -> ThrowsError LispVal
 booleanOrOp l = return $ foldl f (Bool False) l
     where f :: LispVal -> LispVal -> LispVal
           f (Bool False) x = x
           f y _ = y
 
+-- and
 booleanAndOp :: [LispVal] -> ThrowsError LispVal
 booleanAndOp l = return $ foldl f (Bool True) l
     where f :: LispVal -> LispVal -> LispVal
           f (Bool False) _ = Bool False
           f _ y = y
 
+-- helper functions
 extractString :: LispVal -> ThrowsError String
 extractString (String x) = return x
 extractString x = throwError (TypeMismatch "String" x)
