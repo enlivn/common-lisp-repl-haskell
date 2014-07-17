88efbbbdb3f33de2cd7ae39346b309ff0c812906
Ray R <info@enlivn.io>
2014-07-16 21:15:43 -0400

    Adding REPL.

--- src/Main.hs
+++ src/Main.hs
@@ -2,12 +2,51 @@ module Main where
 
 import Parser
 import System.Environment
+import System.IO
 import Types
 import Evaluator
 import Control.Monad (liftM)
 
+outputStr :: String -> IO ()
+outputStr msg = putStr msg >> hFlush stdout
+
+outputStrLn :: String -> IO ()
+outputStrLn msg = putStrLn msg >> hFlush stdout
+
+promptAndReadInput :: String -> IO String
+promptAndReadInput msg = outputStr msg >> getLine
+
+evalExpr :: [String] -> String
+evalExpr str = (extractVal . handleError) $ liftM show $ (readExpr $ str !! 0) >>= eval
+
+evalAndPrintExpr :: [String] -> IO ()
+evalAndPrintExpr = (outputStrLn . evalExpr)
+
+evalRawExpr :: [String] -> String
+evalRawExpr = readExprRaw . (!! 0)
+
+evalAndPrintRawExpr :: [String] -> IO ()
+evalAndPrintRawExpr = (outputStrLn . evalRawExpr)
+
+loop_ :: String -> (String -> Bool) -> IO ()
+loop_ prompt terminatePred = do
+    input <- promptAndReadInput prompt
+    if (terminatePred input) then
+      return ()
+    else do
+      evalAndPrintRawExpr [input]
+      evalAndPrintExpr [input]
+      loop_ prompt terminatePred
+
+runRepl :: IO ()
+runRepl = loop_ "clisp>> " (== "quit")
+
 main :: IO ()
 main = do
   x <- getArgs
-  (putStrLn . readExprRaw . (!! 0)) $ x
-  (putStrLn . extractVal) $ handleError $ liftM show $ (readExpr $ x !! 0) >>= eval
+  case (length x) of
+    0 -> runRepl
+    1 -> do
+            (outputStrLn . evalRawExpr) x
+            (outputStrLn . evalExpr) x
+    _ -> putStrLn "Only 0 or 1 inputs allowed"
