15eee3df0e0ed19e5f09790b39773bc502d19c77
Ray R <info@enlivn.io>
2014-07-29 18:25:21 -0400

    Adding more library functions.

--- src/CommonLib.lisp
+++ src/CommonLib.lisp
@@ -15,7 +15,7 @@
 (defun minusp (num) (> 0 num))
 
 (defun flip (func)
-  (lambda (arg1 arg2) (func arg2 arg1)))
+  (lambda (arg1 arg2) (funcall func arg2 arg1)))
 
 (defun compose (f g)
   (lambda (arg) (f (apply g arg))))
@@ -31,7 +31,42 @@
     acc
     (foldl op (funcall op acc (car l)) (cdr l))))
 
+(defun sum (l) (foldl '+ 0 l))
+
+(defun product (l) (foldl '* 1 l))
+
+(defun length (l)
+  (foldl (lambda (x y) (1+ x))
+         0
+         l))
+
+(defun reverse (l)
+  (foldl (flip 'cons) '() l))
+
 (defun foldr (op acc l)
   (if (null l)
     acc
     (funcall op (car l) (foldr op acc (cdr l)))))
+
+(defun member-helper (pred)
+  (lambda (acc curr)
+          (if (id acc)
+              (cons (car acc) (cons curr (cdr acc)))
+              (if (funcall pred curr)
+                (cons curr acc)
+                acc))))
+
+(defun member (obj l)
+  ((lambda (res)
+          (cons (car res) (reverse (cdr res))))
+  (foldl (member-helper (curry 'eql obj)) '() l)))
+
+(defun assoc-helper (pred)
+  (lambda (acc curr)
+          (if (and (not (id acc))
+                   (funcall pred (car curr)))
+              curr
+              acc)))
+
+(defun assoc (obj l)
+  (foldl (assoc-helper (curry 'eql obj)) '() l))
